d928b7a3e5d7e634f085b3ba5261740b0002f990
bala-amypo <bala@amypo.in>
2025-12-18 08:19:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -23,4 +23,39 @@ public class ValidationEntity{
     @Max(30)
     @Positive(message = "Age must be a positive number")
     private int age;
+public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public Integer getID(){
+        return id;
+    }
+    public void setID(Integer id){
+        this.id=id;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public Date getCreated(){
+        return created;
+    }
+    public void setAge(int Age created){
+        this.created=created;
+    }
+
 }
