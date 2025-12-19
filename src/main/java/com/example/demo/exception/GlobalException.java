68b3982da87080cabcfd224703ca85b329217793
bala-amypo <bala@amypo.in>
2025-12-19 05:39:36 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalException.java
+++ src/main/java/com/example/demo/exception/GlobalException.java
@@ -1,13 +1,13 @@
-package com.example.demo.exception;
+// package com.example.demo.exception;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice;
-public class GlobalException{
+// @RestControllerAdvice;
+// public class GlobalException{
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseBody<String> handleValidationException(ValidationException ex){
-        return new ResponseBody<String>
-    }
-}
+//     @ExceptionHandler(ValidationException.class)
+//     public ResponseBody<String> handleValidationException(ValidationException ex){
+//         return new ResponseBody<String>(ex.getMessage(). HttpStatus.BAS_GATEWAY);
+//     }
+// }
