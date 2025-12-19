7772e6d778dc7d7929f8fd0120bb23b93cd6c65a
bala-amypo <bala@amypo.in>
2025-12-19 05:36:36 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalException.java
+++ src/main/java/com/example/demo/exception/GlobalException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalException{
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseBody<String> handleValidationException(){
-        return new ResponseBody<String>("Validation Error", ex)
+    public ResponseBody<String> handleValidationException(ValidationException ex){
+        return new ResponseBody<String>
     }
 }
