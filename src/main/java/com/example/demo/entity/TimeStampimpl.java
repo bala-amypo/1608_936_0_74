4aa75693051e274549f3730a046e7c300493f415
bala-amypo <bala@amypo.in>
2025-12-19 08:36:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampimpl.java
+++ src/main/java/com/example/demo/entity/TimeStampimpl.java
@@ -1,24 +1,25 @@
-package com.example.demo.entity;
-import jakarta.persistence.*;
-import lombok.*;
-import java.time.LocalDateTime;
+package com.example.demo.repository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.TimeStamp;
+
+@Repository
+public interface TimeStampRepository extends JpaRepository<TimeStamp,Long>{
 
 
-@Entity
-@Data
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-
-public class TimeStampEntity{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-     private Long id;
-     private String name;
-     private String email;
-     private LocalDateTime createAt;
-     private LocalDateTime updateAt;
 }
 
 
+
+
+
+
+package com.example.demo.service;
+
+import com.example.demo.entity.TimeStamp;
+
+public  TimeStampService{
+
+TimeStamp postdata(TimeStamp val);
+
+}
